a89c0980d02464641860198f964210f45499faee
Elisa Mamolo <elisa.mamolo@gmail.com>
2022-02-15 13:29:43 +0000

    now can deploy to hana and have custom handlers

--- packages/bookshop/srv/cat-service.js
+++ packages/bookshop/srv/cat-service.js
@@ -3,8 +3,18 @@ const { Books } = cds.entities
 
 /** Service implementation for CatalogService */
 module.exports = cds.service.impl(function() {
+    //after and before handlers
+    //inline handling
   this.after ('READ', 'Books', each => each.stock > 111 && _addDiscount2(each,11))
+  //or call function
   this.before ('CREATE', 'Orders', _reduceStock)
+  // * means for all methods
+  // no entity is passed so it is for all entities
+  this.before('*', (req) => {
+      console.debug('>>>>>>>' + req.method, req.target.name)
+      req.error(400, 'testing error method on req object');
+      
+  })
 })
 
 /** Add some discount for overstocked books */
